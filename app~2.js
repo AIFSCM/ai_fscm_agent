c5de1e9bef2553c088d4a43e9a25dd1a045636f8
LHE Muhammad Ahmad <muhammad.ahmad@raaziqint.com>
2026-05-05 16:19:42 +0500

    Initial commit

---
+++ app~2.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+
+// Health check
+app.get("/", (req, res) => {
+  res.send("Fusion AI Agent is running 🚀");
+});
+
+// Chat endpoint
+app.post("/chat", async (req, res) => {
+  try {
+    //const userMessage = req.body.message;
+const message = req.body.message;
+
+    const response = await axios.post(
+      `${process.env.FUSION_URL}/orchestrator/agent/v2/${process.env.AGENT_TEAM_ID}/invokeAsync`,
+      {
+        message: userMessage
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    res.json(response.data);
+
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).json({
+      error: "Something went wrong"
+    });
+  }
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(400).json({ error: "Invalid JSON format" });
+});
